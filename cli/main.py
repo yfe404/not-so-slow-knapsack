7e07ca7fed2357280b22eaa1e668688aec61be07
yafeunteun <yannfeunteun@gmail.com>
2019-01-28 18:03:51 +0100

    run solver on chosen instance

--- cli/main.py
+++ cli/main.py
@@ -1,14 +1,23 @@
 import os
 import click
+import subprocess
 
+
+FILES = [ name for name in os.listdir("./data") ]
 
 def print_menu():
-    files = [ name for name in os.listdir("../data") ]
-
-    for idx, name in enumerate(files):
+    for idx, name in enumerate(FILES):
         print("{}) {}".format(idx+1, name))
 
     print()
+
+def solve(instance_id):
+    filepath = "./data/" + FILES[instance_id - 1]
+    print(filepath)
+    p = subprocess.run(["./bin/knapsack", filepath], stdout=subprocess.PIPE)
+    output_data = p.stdout.decode("utf-8").strip()
+
+    return output_data
 
 
 if __name__ == "__main__":
@@ -16,3 +25,5 @@ if __name__ == "__main__":
     print("Choose a problem instance:\n")
     print_menu()
     value = click.prompt('Please enter a valid integer', type=int)
+
+    print(solve(value))
